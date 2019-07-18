2262e43935db232621b4bfdff1e43f84c71fa4ee
Jénaïc Cambré <me@jenaiccambre.com>
2019-07-17 19:41:07 -0500

    remove old code

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.prepare().then(() => {
     const { pathname, query } = parsedUrl;
     if (pathname === "/") {
       renderHomepage(app, req, res, parsedUrl, filepath => {
-        console.log("pathname /");
+        // console.log("pathname /");
         app.serveStatic(req, res, filepath);
       });
     } else if (pathname === "/robots.txt") {
@@ -63,38 +63,4 @@ app.prepare().then(() => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${PORT}`);
   });
-  // createServer((req, res) => {
-  //   // Be sure to pass `true` as the second argument to `url.parse`.
-  //   // This tells it to parse the query portion of the URL.
-  //   const parsedUrl = parse(req.url, true);
-  //   const { pathname, query } = parsedUrl;
-  //   if (pathname === "/") {
-  //     renderHomepage(app, req, res, parsedUrl, filepath => {
-  //       // console.log("pathname /");
-  //       app.serveStatic(req, res, filepath);
-  //     });
-  //   } else if (pathname === "/posts") {
-  //     app.render(req, res, "/allPosts", query);
-  //   } else if (pathname.substr(0, 6) === "/posts") {
-  //     const postId = pathname.split("/")[2];
-  //     renderPost(postId, app, req, res, parsedUrl, filepath => {
-  //       // app.render(req, res, "/post", query);
-  //       app.serveStatic(req, res, filepath);
-  //     });
-  //   } else if (pathname.substr(0, 14) === "/static/posts/") {
-  //     toJpg(pathname, () => {
-  //       handle(req, res, parsedUrl);
-  //     });
-  //   } else if (pathname === "/caches/") {
-  //     console.log("okokok");
-  //     cacheViewer(filepath => {
-  //       app.serveStatic(req, res, filepath);
-  //     });
-  //   } else {
-  //     handle(req, res, parsedUrl);
-  //   }
-  // }).listen(PORT, err => {
-  //   if (err) throw err;
-  //   console.log(`> Ready on http://localhost:${PORT}`);
-  // });
 });
