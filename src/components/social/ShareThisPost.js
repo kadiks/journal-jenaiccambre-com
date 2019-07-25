37325762c83d6bb6b20fb520ce0d5e8841dc84ed
Jénaïc Cambré <me@jenaiccambre.com>
2019-07-25 14:04:00 -0500

    fix class warning

--- src/components/social/ShareThisPost.js
+++ src/components/social/ShareThisPost.js
@@ -2,7 +2,7 @@ export default () => {
   return (
     <div className="row">
       <div className="col-10 offset-1 col-md-8 offset-md-2">
-        <div class="sharethis-inline-share-buttons" />
+        <div className="sharethis-inline-share-buttons" />
       </div>
     </div>
   );
